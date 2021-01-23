9525b237620559711b79f585b3fc45fdc155df63
ahmedloll <ahmed.alaa.fathy.96@gmail.com>
2021-01-23 19:24:40 +0200

    made the header more responsive and made a drop down menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -16,7 +16,19 @@
         <li><i class="fas fa-user"></i><span> Sign in</span></li>
         <li><i class="far fa-heart"></i><span> Wish lists</span></li>
         <li><i class="fas fa-shopping-cart"></i><span> Cart</span></li>
-        <li class="menu-bar"><i class="fas fa-bars"></i></li>
+        <li class="menu-bar pointer dropdown">
+          <i class="fas fa-bars"></i>
+          <div class=" dropdown-content">
+            <ul>
+              <li>About Us</li>
+              <li>Categories</li>
+              <li>Culture</li>
+              <li>Latest News</li>
+              <li>Careers</li>
+              <li>Contact us</li>
+            </ul>
+          </div>
+        </li>
       </ul>
     </div>
   </header>
@@ -25,6 +37,9 @@
 <script>
   export default {
     name: "Header",
+    data() {
+      return { isActive: true };
+    },
   };
 </script>
 
@@ -35,6 +50,7 @@
   }
   h1 {
     margin: 0;
+    font-size: 2em;
   }
   h1 i {
     color: #3c1b44;
@@ -45,6 +61,19 @@
 
   li {
     margin-left: 10px;
+  }
+
+  .dropdown-content {
+    display: none;
+    position: absolute;
+    right: 16.9270833%;
+    background-color: #f9f9f9;
+    min-width: 120px;
+    box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+    z-index: 1;
+  }
+  .dropdown:hover .dropdown-content {
+    display: block;
   }
 
   @media only screen and (min-width: 1350px) {
@@ -60,6 +89,22 @@
   @media only screen and (max-width: 700px) {
     span {
       display: none;
+    }
+  }
+  @media only screen and (max-width: 460px) {
+    h1 {
+      font-size: 1.5em;
+    }
+    i {
+      font-size: 1em;
+    }
+  }
+  @media only screen and (max-width: 373px) {
+    h1 {
+      font-size: 1em;
+    }
+    i {
+      font-size: 0.75em;
     }
   }
 </style>
